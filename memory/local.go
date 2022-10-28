4505c35ad8a3a7a337ce14e2a8911030b345303d
Sagleft <sagleft@inbox.ru>
2022-10-29 01:26:00 +0300

    separate file

---
+++ memory/local.go
@@ -0,0 +1,35 @@
+package memory
+
+import (
+	"fmt"
+
+	swissknife "github.com/Sagleft/swiss-knife"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type localDB struct {
+	conn *gorm.DB
+}
+
+func NewLocalDB(filename string) (Memory, error) {
+	if !swissknife.IsFileExists(filename) {
+		return nil, fmt.Errorf("db file not found: %q", filename)
+	}
+
+	db, err := gorm.Open(sqlite.Open(filename), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	for _, prefab := range models {
+		if err := db.AutoMigrate(prefab); err != nil {
+			return nil, fmt.Errorf("failed to migrate: %w", err)
+		}
+	}
+
+	return &localDB{
+		conn: db,
+	}, nil
+}
--- memory/memory.go
+++ memory/memory.go
@@ -1,37 +1,3 @@
 package memory
 
-import (
-	"fmt"
-
-	swissknife "github.com/Sagleft/swiss-knife"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-)
-
 type Memory interface{}
-
-type defaultDB struct {
-	conn *gorm.DB
-}
-
-func NewLocalDB(filename string) (Memory, error) {
-	if !swissknife.IsFileExists(filename) {
-		return nil, fmt.Errorf("db file not found: %q", filename)
-	}
-
-	db, err := gorm.Open(sqlite.Open(filename), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	// Migrate the schema
-	for _, prefab := range models {
-		if err := db.AutoMigrate(prefab); err != nil {
-			return nil, fmt.Errorf("failed to migrate: %w", err)
-		}
-	}
-
-	return &defaultDB{
-		conn: db,
-	}, nil
-}
