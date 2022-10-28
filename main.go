c698d66fcf4cfcc0b9ea607a2514926be1cdfa38
Sagleft <sagleft@inbox.ru>
2022-10-29 00:50:34 +0300

    add db field

--- main.go
+++ main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"bot/memory"
 	"bot/messenger"
 	"log"
 
 	utopiago "github.com/Sagleft/utopialib-go"
+	"gorm.io/gorm"
+)
+
+const (
+	dbFilename = "memory.db"
 )
 
 func main() {
-	if err := newBot().run(); err != nil {
+	db, err := memory.InitDB(dbFilename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := newBot(db).run(); err != nil {
 		log.Fatalln(err)
 	}
 }
 
 type bot struct {
+	DB        *gorm.DB
 	Messenger messenger.Messenger
 }
 
-func newBot() *bot {
+func newBot(db *gorm.DB) *bot {
 	return &bot{
+		DB:        db,
 		Messenger: messenger.NewUtopiaMessenger(utopiago.UtopiaClient{}),
 	}
 }
